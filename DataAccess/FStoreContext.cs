d8e59f107b80b8b062521d3db21214d6c2e64371
Nhat Tan <tanln01072001@gmail.com>
2024-03-13 02:17:31 +0700

    done

--- DataAccess/FStoreContext.cs
+++ DataAccess/FStoreContext.cs
@@ -27,7 +27,7 @@ namespace DataAccess
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-E62CFAN;uid=sa;pwd=1;database=SalesManagement;Integrated Security=True; TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=DESKTOP-E62CFAN;uid=sa;pwd=1;database=Ass3;Integrated Security=True; TrustServerCertificate=True");
             }
         }
 
